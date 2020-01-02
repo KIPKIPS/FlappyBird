2a1120be01a9a5841ec42f9aaae1c99f0c309cd6
RECNEPS-KIPS <2432719656@qq.com>
2020-01-02 16:53:40 +0800

    FlappyBird项目上传

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    private Rigidbody2D rigidbody;
+    private PlayMakerFSM fsm;
+
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+        fsm = GetComponent<PlayMakerFSM>();
+    }
+
+    public void AddForce(Vector2 force) {
+        rigidbody.velocity = new Vector2(0, 0); //先将物体的向下速度设置为0;
+        rigidbody.AddForce(force);
+    }
+
+    public void OnCollisionEnter2D(Collision2D other) { //发送Over事件
+        fsm.SendEvent("Over");
+    }
+}
